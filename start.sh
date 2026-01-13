6c5d9ce26cde73c1fdcf06843c04ee0d2812e581
paulkisilu40-eng <paulkisilu40@gmail.com>
2026-01-13 08:31:44 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-cd DeepMindCheck
-python manage.py collectstatic --noinput
-python manage.py migrate
-gunicorn deepmindcheck.wsgi:application
+# Check if we're already in the right directory
+if [ -f "manage.py" ]; then
+    echo "Found manage.py in current directory"
+    python manage.py collectstatic --noinput
+    python manage.py migrate
+    gunicorn deepmindcheck.wsgi:application
+elif [ -f "DeepMindCheck/manage.py" ]; then
+    echo "Found manage.py in DeepMindCheck subdirectory"
+    cd DeepMindCheck
+    python manage.py collectstatic --noinput
+    python manage.py migrate
+    gunicorn deepmindcheck.wsgi:application
+else
+    echo "Cannot find manage.py!"
+    find . -name "manage.py"
+    exit 1
+fi
